179bb44494196d9d6cf9f567063fc9467e3ac148
Moritz Reiter <moritz@anothernode.com>
2018-11-08 19:42:08 +0100

    Polish the code a bit

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
+// Only squares possible for now
+let size = 30;
+
 let gridDiv = document.getElementById("grid");
 let nextButton = document.getElementById("next");
-
-let size = 15;
-
 let grid = [];
-for (let y = 0; y < size; y++) {
-    grid[y] = [];
-    for (let x = 0; x < size; x++) {
-        grid[y][x] = Math.floor(Math.random() * 2) == 0;
-   }
-}
 
+initGrid();
 drawGrid();
 
 nextButton.addEventListener("click", () => {
@@ -18,57 +13,12 @@ nextButton.addEventListener("click", () => {
     drawGrid();
 });
 
-function nextGeneration() {
-    let next = [];
+function initGrid() {
     for (let y = 0; y < size; y++) {
-        next[y] = [];
+        grid[y] = [];
         for (let x = 0; x < size; x++) {
-            let neighbors = countNeighbors(y, x);
-            if (grid[y][x] == true && neighbors < 2 || neighbors > 3) {
-                next[y][x] = false;
-            } else if (grid[y][x] == true && neighbors >= 2 && neighbors <= 3) {
-                next[y][x] = true;
-            } else if (grid[y][x] == false && neighbors == 3) {
-                next[y][x] = true;
-            } else {
-                next[y][x] = grid[y][x]
-            }
+            grid[y][x] = Math.floor(Math.random() * 2) == 0;
         }
-    }
-    grid = next;
-}
-
-/*
-| x - 1, y - 1 | x, y - 1 | x + 1, y - 1 |
-| x - 1, y     | x, y     | x + 1, y     |
-| x - 1, y + 1 | x, y + 1 | x + 1, y + 1 |
-*/
-function countNeighbors(y, x) {
-    let counter = 0;
-    y -= 1;
-    if (alive(y, x)) counter++;
-    x += 1;
-    if (alive(y, x)) counter++;
-    y += 1;
-    if (alive(y, x)) counter++;
-    y += 1;
-    if (alive(y, x)) counter++;
-    x -= 1;
-    if (alive(y, x)) counter++;
-    x -= 1;
-    if (alive(y, x)) counter++;
-    y -= 1;
-    if (alive(y, x)) counter++;
-    y -= 1;
-    if (alive(y, x)) counter++;
-    return counter;
-}
-
-function alive(y, x) {
-    if (y < 0 || y > size - 1 || x < 0 || x > size - 1) {
-        return false;
-    } else {
-        return grid[y][x];
     }
 }
 
@@ -92,5 +42,64 @@ function drawGrid() {
             gridDiv.appendChild(checkbox);
         }
         gridDiv.appendChild(document.createElement("br"));
+    }
+}
+
+function nextGeneration() {
+    let next = [];
+    for (let y = 0; y < size; y++) {
+        next[y] = [];
+        for (let x = 0; x < size; x++) {
+            let neighbors = countNeighbors(x, y);
+            if (grid[y][x] == true && neighbors < 2 || neighbors > 3) {
+                next[y][x] = false;
+            } else if (grid[y][x] == true && neighbors >= 2 && neighbors <= 3) {
+                next[y][x] = true;
+            } else if (grid[y][x] == false && neighbors == 3) {
+                next[y][x] = true;
+            } else {
+                next[y][x] = grid[y][x]
+            }
+        }
+    }
+    grid = next;
+}
+
+/*
+Count the neighbors of cell (x, y) starting with cell (x, y - 1)
+------------------------------------------
+| x - 1, y - 1 | x, y - 1 | x + 1, y - 1 |
+------------------------------------------
+| x - 1, y     | x, y     | x + 1, y     |
+------------------------------------------
+| x - 1, y + 1 | x, y + 1 | x + 1, y + 1 |
+------------------------------------------
+*/
+function countNeighbors(x, y) {
+    let counter = 0;
+    y -= 1;
+    if (alive(x, y)) counter++;
+    x += 1;
+    if (alive(x, y)) counter++;
+    y += 1;
+    if (alive(x, y)) counter++;
+    y += 1;
+    if (alive(x, y)) counter++;
+    x -= 1;
+    if (alive(x, y)) counter++;
+    x -= 1;
+    if (alive(x, y)) counter++;
+    y -= 1;
+    if (alive(x, y)) counter++;
+    y -= 1;
+    if (alive(x, y)) counter++;
+    return counter;
+}
+
+function alive(x, y) {
+    if (y < 0 || y > size - 1 || x < 0 || x > size - 1) {
+        return false;
+    } else {
+        return grid[y][x];
     }
 }
